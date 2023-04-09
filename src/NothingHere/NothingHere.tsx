c6686627afe9b3e069527debfb233d707241ecff
Manjari <85077494+Manjari746@users.noreply.github.com>
2023-04-09 21:00:46 +0530

    Delete src/NothingHere directory

--- src/NothingHere/NothingHere.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import style from './NothingHere.module.scss';
-
-const NothingHere: React.FunctionComponent = () => {
-  return <p className={style.nothingHere}>There's nothing here so far...</p>
-}
-
-export default NothingHere;
